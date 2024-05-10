f09f07e039c7d702c1699cf92cf23e76d28b1386
pushpenderthakur <mailto:parukh11@gmail.com>
2024-05-10 15:56:06 +0530

    created the user page

--- src/js/custome.js
+++ src/js/custome.js
@@ -8,7 +8,16 @@ $('.main-slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1
   });
-
+  $('.product-slider').slick({
+    infinite: true,
+    dots: false,
+    autoplay: false,
+    speed: 700,
+    arrows: true,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
   $('.toggle-box a').click(function() {
     $('.menu-box').toggle('slow');
 });

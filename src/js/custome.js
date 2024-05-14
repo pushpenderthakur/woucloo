1711eccf0c4efd5470ae94d6efa5c503a1dfb1f1
pushpenderthakur <mailto:parukh11@gmail.com>
2024-05-14 18:27:18 +0530

    Created ask trader and filter popup and product page with filter

--- src/js/custome.js
+++ src/js/custome.js
@@ -21,3 +21,34 @@ $('.main-slider').slick({
   $('.toggle-box a').click(function() {
     $('.menu-box').toggle('slow');
 });
+
+$('.filter-box a').click(function() {
+  $('.featured-fislter-box').toggle('slow');
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const askTraderButtons = document.querySelectorAll(".btn-Seashell.ask-trader");
+  const popupProducts = document.querySelectorAll(".popup-product");
+  const closeAskTradeButtons = document.querySelectorAll(".close-ask-trade");
+
+  askTraderButtons.forEach((button, index) => {
+      button.addEventListener("click", function(event) {
+          event.preventDefault(); // Prevent the default behavior of the anchor tag
+          
+          // Hide all popup products
+          popupProducts.forEach(popup => {
+              popup.style.display = "none";
+          });
+
+          // Show the corresponding popup product
+          popupProducts[index].style.display = "block";
+      });
+  });
+
+  closeAskTradeButtons.forEach((button, index) => {
+      button.addEventListener("click", function(event) {
+          event.preventDefault();
+          popupProducts[index].style.display = "none";
+      });
+  });
+});
